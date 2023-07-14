8c86fcd21356c97f657b8b7dbcfa49fe261a017e
jhonatanprez <jhonatan.perez@fundacion-jala.org>
2023-07-13 23:03:56 -0500

    class 1: dinosaur

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,3 +1,14 @@
+
+class Dinosaur:
+    def __init__(self):
+        pass
+    
+    def update():
+        pass
+    
+    def draw():
+        pass
+
 # create class Dinosaur
 # add __init__
 # add game loop methods (update, draw)
